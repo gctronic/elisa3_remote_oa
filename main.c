ec363acc763be5dcbe3aa7e446ae402b47a8b85c
stefano <stefano@337712d9-d4f5-6d42-b37f-cd6ca520555a>
2014-06-18 12:29:13 +0000

    - change the code to reflects library rev164 - re-organized the code to be more clean: created functions "avoidObstacles" and "updateRGB" that reduce the lines of code in the main
    git-svn-id: file:///P/lavoro/local-repo/elisaIII/pc-side/library/pc-side-elisa3-library-obstacle-avoidance@167 337712d9-d4f5-6d42-b37f-cd6ca520555a

--- main.c
+++ main.c
@@ -2,78 +2,95 @@
 #include <sys/types.h>
 #include <time.h>
 #include "elisa3-lib.h"
-
 #ifdef _WIN32
     #include "windows.h"
 #endif
 
-#define OBSTACLE_THR 50 //170
+#define OBSTACLE_THR 50
 
-// received from robot
-int robotAddress;
-unsigned int robProx[8] = {0};
+unsigned char updateRGB(char *red, char *green, char *blue) {
+    static unsigned int i=0;
+    unsigned int rndNum;
 
-// sent to robot
-char robLSpeed=0, robRSpeed=0;
-char robRedLed=0, robGreenLed=0, robBlueLed=0;
+    i = (i+1)%65000;  // use to change the rgb leds
+    if(i==0) {
+        rndNum = rand()%400;
+        if(rndNum < 100) {
+            *red = rand()%100;
+            *green = rand()%100;
+            *blue = 0;
+        } else if(rndNum < 200) {
+            *red = rand()%100;
+            *green = 0;
+            *blue = rand()%100;
+        } else if(rndNum < 300) {
+            *red = 0;
+            *green = rand()%100;
+            *blue = rand()%100;
+        } else {
+            *red = rand()%100;
+            *green = rand()%100;
+            *blue = rand()%100;
+        }
+        return 1;
+    }
+    return 0;
+}
 
-// various
-int rightProxSum=0, leftProxSum=0;	// sum of proximity values on right or left
-unsigned int i;
+void avoidObstacles(unsigned int *prox, char *left, char *right) {
+    int rightProxSum=0, leftProxSum=0;	// sum of proximity values on right or left
+
+    // obstacle avoidance using the 3 front proximity sensors
+    rightProxSum = prox[0]/2 + prox[1];
+    leftProxSum = prox[0]/2 + prox[7];
+
+    rightProxSum /= 5;                 // scale the sum to have a moderate impact on the velocity
+    leftProxSum /= 5;
+    if(rightProxSum > 60) {             // velocity of the motors set to be from -30 to +30
+        rightProxSum = 60;
+    }
+    if(leftProxSum > 60) {
+        leftProxSum = 60;
+    }
+    *right = 30-leftProxSum;     // set the speed to the motors
+    *left = 30-rightProxSum;
+
+}
 
 int main(void) {
 
-    srand ( time(NULL) );
+    int robotAddress[1];
+    // received from robot
+    unsigned int robProx[8] = {0};
+    // sent to robot
+    char robLSpeed=0, robRSpeed=0;
+    char robRedLed=0, robGreenLed=0, robBlueLed=0;
+
+    srand(time(NULL));
 
     printf("\r\nInsert the robot address: ");
-    scanf("%d", &robotAddress);
+    scanf("%d", &robotAddress[0]);
 
-    // init the communication with the RF module and thus with the robot
-    openRobotComm();
-
-    // set the address of the robot to control; if more robots (max of 4 per packet) need to be controlled
-    // the function need to be called more times with different ids (0..3) and addresses
-    setRobotAddress(0, robotAddress);
-
-    resetFlagTX(robotAddress);  // no onboard obstacle avoidance
-
-    i=0;
+    // init the communication with the robots; set the addresses and number of the robots to control
+    startCommunication(robotAddress, 1);
 
     while(1) {
 
-        i = (i+1)%100;  // use to change the rgb leds
-        if(i==0) {
-            robRedLed = rand()%100;
-            robGreenLed = rand()%100;
-            robBlueLed = rand()%100;
+        getAllProximity(robotAddress[0], robProx);
+
+        avoidObstacles(robProx, &robLSpeed, &robRSpeed);
+        setLeftSpeed(robotAddress[0], robLSpeed);
+        setRightSpeed(robotAddress[0], robRSpeed);
+
+        if(updateRGB(&robRedLed, &robGreenLed, &robBlueLed)) {
+            setRed(robotAddress[0], robRedLed);
+            setGreen(robotAddress[0], robGreenLed);
+            setBlue(robotAddress[0], robBlueLed);
         }
-
-        getAllProximity(robotAddress, robProx);
-
-        // obstacle avoidance using the 3 front proximity sensors
-        rightProxSum = robProx[0]/2 + robProx[1];
-        leftProxSum = robProx[0]/2 + robProx[7];
-
-        rightProxSum /= 10;                 // scale the sum to have a moderate impact on the velocity
-        leftProxSum /= 10;
-        if(rightProxSum > 60) {             // velocity of the motors goes from -30 to +30
-            rightProxSum = 60;
-        }
-        if(leftProxSum > 60) {
-            leftProxSum = 60;
-        }
-        robRSpeed = 30-leftProxSum;     // set the speed to the motors
-        robLSpeed = 30-rightProxSum;
-
-        setLeftSpeed(robotAddress, robLSpeed);
-        setRightSpeed(robotAddress, robRSpeed);
-        setRed(robotAddress, robRedLed);
-        setGreen(robotAddress, robGreenLed);
-        setBlue(robotAddress, robBlueLed);
 
     }
 
-    closeRobotComm();
+    stopCommunication();
 
 	return 0;
 
